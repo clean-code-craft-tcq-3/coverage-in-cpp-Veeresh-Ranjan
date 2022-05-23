c43a25a54e195c44571536326de414456d76d6a4
Veeresh-Ranjan <99407299+Veeresh-Ranjan@users.noreply.github.com>
2022-05-24 01:13:47 +0530

    Add files via upload

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -2,9 +2,20 @@
 
 #include "test/catch.hpp"
 #include "typewise-alert.h"
+#include "string.h"
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
+  BatteryCharacter batteryChar;
+  batteryChar.coolingType = HI_ACTIVE_COOLING;
+  strcpy(batteryChar.brand,"Ameron");
+  checkAndAlert(TO_EMAIL, batteryChar, 46);
+  BatteryCharacter batteryChar2;
+  batteryChar2.coolingType = MED_ACTIVE_COOLING;
+  strcpy(batteryChar2.brand,"Exide");
+  checkAndAlert(TO_CONTROLLER, batteryChar2, 41);
+  sendToEmail(TOO_HIGH);
+  sendToController(TOO_LOW);
 }
 
